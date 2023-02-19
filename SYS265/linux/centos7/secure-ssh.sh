1a2daf7f26059ff3ff9b6bca20a0c2eccd41c090
root <root@web01-janelle.janelle.local>
2023-02-19 17:55:17 -0500

    Secure ssh file

---
+++ SYS265/linux/centos7/secure-ssh.sh
@@ -0,0 +1,6 @@
+#secure-ssh.sh
+#author Janelle Fisher aka IronMansDaughter
+#creates a new ssh user using $1 parameter
+#adds a public key from the local repo pr curled from the remote repo
+#removes all ability to ssh in (you do not need to disable root ssh on ubuntu bc it is already like that)
+echo "ALL YOU CODE GOES HERE"
