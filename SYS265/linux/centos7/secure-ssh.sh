6883d0dc8ea9f07b27c4161c5ca80e5ff26830d3
IronMansDaughter <janelle.fisher@mymail.Champlain.edu>
2023-02-20 01:57:27 +0000

    New revised ssh file

--- SYS265/linux/centos7/secure-ssh.sh
+++ SYS265/linux/centos7/secure-ssh.sh
@@ -3,4 +3,15 @@
 #creates a new ssh user using $1 parameter
 #adds a public key from the local repo pr curled from the remote repo
 #removes all ability to ssh in (you do not need to disable root ssh on ubuntu bc it is already like that)
-echo "ALL YOU CODE GOES HERE"
+#!/bin/bash
+echo "Hello, what would you like your ussername to be?"
+read first_name
+
+USER=$first_name
+
+sudo useradd -m -d /home/${USER} -s /bin/bash ${USER};
+sudo mkdir /home/${USER}/.ssh;
+sudo cp ~/Sys-265/SYS265/linux/public-keys/id_rsa.pub /home/${USER}/.ssh/authorized_keys;
+sudo chmod 700 /home/${USER}/.ssh;
+sudo chmod 600 /home/${USER}/.ssh/authorized_keys;
+sudo chown -R ${USER}:${USER} /home/${USER}/.ssh;
